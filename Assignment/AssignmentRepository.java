2e9a4f3b7f6f7c14473e531e98be5f74006dd14b
Nafiz Shahriar <shahriarsami123@gmail.com>
2026-07-18 05:23:27 -0700

    Add AssignmentRepository interface for assignment management

--- Assignment/AssignmentRepository.java
+++ Assignment/AssignmentRepository.java
@@ -1,1 +1,8 @@
+package com.campusconnect.assignment;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
+    List<Assignment> findAllByOrderByDueDateAsc();
+}
