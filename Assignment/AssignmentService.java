625fce585bc52c16a38c1d426d9b9e5f8e031d11
Nafiz Shahriar <shahriarsami123@gmail.com>
2026-07-18 05:23:42 -0700

    Implement AssignmentService with CRUD methods

--- Assignment/AssignmentService.java
+++ Assignment/AssignmentService.java
@@ -1,1 +1,22 @@
+package com.campusconnect.assignment;
 
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class AssignmentService {
+
+    private final AssignmentRepository repository;
+
+    public AssignmentService(AssignmentRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Assignment> getAllSortedByDueDate() {
+        return repository.findAllByOrderByDueDateAsc();
+    }
+
+    public Assignment create(Assignment assignment) {
+        return repository.save(assignment);
+    }
+}
