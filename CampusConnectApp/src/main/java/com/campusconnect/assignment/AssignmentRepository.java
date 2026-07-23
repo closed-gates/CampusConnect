3f429c4423d1554d7c98e4b263cf43ce6bf7f646
Nafiz Shahriar <shahriarsami123@gmail.com>
2026-07-23 08:52:04 -0700

    Delete CampusConnectApp directory

--- CampusConnectApp/src/main/java/com/campusconnect/assignment/AssignmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.campusconnect.assignment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
-    List<Assignment> findAllByOrderByDueDateAsc();
-    List<Assignment> findByCourseCode(String courseCode);
-}
